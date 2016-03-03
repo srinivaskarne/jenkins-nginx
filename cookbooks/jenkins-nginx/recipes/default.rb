80967dedb4f1c591a2d0d21e3bf4928a11ca7692
srinivaskarne <srinivasu.karne@gmail.com>
2016-03-03 12:58:47 +0530

    comments added to the recipe

--- cookbooks/jenkins-nginx/recipes/default.rb
+++ cookbooks/jenkins-nginx/recipes/default.rb
@@ -1,3 +1,8 @@
+#Installing Jenkins and Nginx as reverse proxy with ssl certificate
+
+#1st command in bash block downloads the key from the http link and adds it to apt-key list
+#2nd command adds link(from which jenkins has to be downloaded) to apt sources.list
+#3rd command will redownload broken apt packages if any
 bash 'exe' do
 code <<-EOH
 sudo wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
@@ -7,28 +12,36 @@ sudo apt-get update
 EOH
 end
 
+#Download dependency packages for jenkins like jre and jdk
 package "openjdk-7-jre" 
-
 package "openjdk-7-jdk"
 
+#This will remove any broken dpkg packages
 execute 'dpkg' do
 command 'sudo dpkg --configure -a'
 end
 
+
+#install jenkins package
 package "jenkins"
 
+#It will enable and start jenkins service
 service "jenkins" do
   supports [:stop, :start, :restart]
   action [:enable, :start]
 end
 
+#install nginx package
 package 'nginx'
 
+#It will enable and start nginx service
 service "nginx" do
  supports status: true
  action [:enable, :start]
 end
 
+#Modify default Nginx configuration file for reverse proxy settings
+#Give permissions to modify the file
 template '/etc/nginx/sites-enabled/default' do
 source 'nginx.erb'
 cookbook 'jenkins-nginx'
@@ -39,33 +52,41 @@ mode '0755'
 action:create
 end
 
+
+#Comments the JENKINS_ARGS line and adding new line to the jenkins file 
 bash 'key' do
 code <<-EOH
 sudo chmod 0755 /etc
-echo 'JENKINS_ARGS="--webroot=/var/cache/jenkins/war --httpListenAddress=192.168.8.68 --httpPort=$HTTP_PORT -ajp13Port=$AJP_PORT"' >> /etc/default/jenkins
+sed -i 's/JENKINS_ARGS/#JENKINS_ARGS/' /etc/default/jenkins 
+echo 'JENKINS_ARGS="--webroot=/var/cache/jenkins/war --httpListenAddress=127.0.0.1 --httpPort=$HTTP_PORT -ajp13Port=$AJP_PORT"' >> /etc/default/jenkins
 EOH
 end
-ssl_keyfile = '/etc/nginx/cert.key'
-ssl_crtfile = '/etc/nginx/cert.crt'
-ssl_signing_conf = '/etc/nginx/cert-ssl.conf'
 
-#ssl_keyfile = "#{node['nginx']['dir']}.key"
-#ssl_crtfile = "#{node['nginx']['dir']}.crt"
-#ssl_signing_conf = "#{node['nginx']['dir']}-ssl.conf"
-unless File.exists?(ssl_keyfile) && File.exists?(ssl_crtfile) && File.exists?(ssl_signing_conf)
-  file ssl_keyfile do
+
+
+keyfile = '/etc/nginx/cert.key'
+crtfile = '/etc/nginx/cert.crt'
+sslconfig = '/etc/nginx/sslcert.conf'
+
+#If any file doesn't exist it will create all the three files 
+
+unless File.exists?(keyfile) && File.exists?(crtfile) && File.exists?(sslconfig)
+
+#Generate a 2048-bit RSA key and will store into cert.key file
+  file keyfile do
     owner "root"
     group "root"
     mode "0644"
     content `/opt/chef/embedded/bin/openssl genrsa 2048`
-    not_if { File.exists?(ssl_keyfile) }
+    not_if { File.exists?(keyfile) }
   end
 
-  file ssl_signing_conf do
+#SSL configuration file which will store all the private data
+  file sslconf do
     owner "root"
     group "root"
     mode "0644"
-    not_if { File.exists?(ssl_signing_conf) }
+    not_if { File.exists?(sslconfig) }
     content <<-EOH
   [ req ]
   distinguished_name = req_distinguished_name
@@ -81,24 +102,33 @@ unless File.exists?(ssl_keyfile) && File.exists?(ssl_crtfile) && File.exists?(ss
   EOH
   end
 
+#Generate a selfsigned Certificate  
   ruby_block "create crtfile" do
     block do
-      r = Chef::Resource::File.new(ssl_crtfile, run_context)
+      r = Chef::Resource::File.new(crtfile, run_context)
       r.owner "root"
       r.group "root"
       r.mode "0755"
-      r.content `/opt/chef/embedded/bin/openssl req -config '#{ssl_signing_conf}' -new -x509 -nodes -sha1 -days 3650 -key #{ssl_keyfile}`
-      r.not_if { File.exists?(ssl_crtfile) }
+      r.content `/opt/chef/embedded/bin/openssl req -config '#{sslconfig}' -new -x509 -nodes -sha1 -days 3650 -key #{keyfile}`
+      r.not_if { File.exists?(crtfile) }
       r.run_action(:create)
     end
   end
 end
 
-node.default['nginx']['ssl_certificate'] ||= ssl_crtfile
-node.default['nginx']['ssl_certificate_key'] ||= ssl_keyfile
+#It will enable and restart the jenkins service
+service "jenkins" do
+  supports [:stop, :start, :restart]
+  action [:enable, :restart]
+end
 
-#
-# Cookbook Name:: jenkins-nginx
-# Recipe:: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+#It will enable and restart nginx service
+service "nginx" do
+ supports status: true
+ action [:enable, :restart]
+end
+
+#It will store sslcrt file and sslkey file details to node details.
+node.default['private_chef']['nginx']['ssl_certificate'] ||= crtfile
+node.default['private_chef']['nginx']['ssl_certificate_key'] ||= keyfile
+eserved.
